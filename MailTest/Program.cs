d6ce2e865e8bf31b868156e7b11deaacf896a101
HasiBul Islam ShanTo <bdshanto@hotmail.com>
2022-06-13 20:56:06 +0600

    Add project files.

---
+++ MailTest/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+public class EmailModel
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public IFormFile Attachment { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
